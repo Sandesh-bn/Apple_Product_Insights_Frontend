5c7778dc1265749f638aa52483e4e888f2b88878
A B <sandesh647bn@gmail.com>
2026-05-18 13:49:49 -0700

    fix navbar bug

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -49,7 +49,10 @@ export default function TopNav() {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md  transition-colors duration-300">
+      <nav 
+        className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md transition-colors duration-300"
+        style={{ paddingRight: "var(--removed-body-scroll-bar-size, 0px)" }}
+      >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-14">
             
